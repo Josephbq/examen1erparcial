408931916cf5b1cc99ee826ce475f47a10a72625
barra <barra@LAPTOP-U81S3TP5>
2021-09-14 14:36:20 -0400

    creacion de views

---
+++ Controllers/GastoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1;
+
+namespace WebApplication1.Controllers
+{
+    public class GastoesController : Controller
+    {
+        private Model1Container db = new Model1Container();
+
+        // GET: Gastoes
+        public ActionResult Index()
+        {
+            return View(db.GastoSet.ToList());
+        }
+
+        // GET: Gastoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gasto gasto = db.GastoSet.Find(id);
+            if (gasto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gasto);
+        }
+
+        // GET: Gastoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Gastoes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Fecha,Cantidad,CategoriaID,MonedaID,LugarID")] Gasto gasto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.GastoSet.Add(gasto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(gasto);
+        }
+
+        // GET: Gastoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gasto gasto = db.GastoSet.Find(id);
+            if (gasto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gasto);
+        }
+
+        // POST: Gastoes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Fecha,Cantidad,CategoriaID,MonedaID,LugarID")] Gasto gasto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(gasto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(gasto);
+        }
+
+        // GET: Gastoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gasto gasto = db.GastoSet.Find(id);
+            if (gasto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gasto);
+        }
+
+        // POST: Gastoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Gasto gasto = db.GastoSet.Find(id);
+            db.GastoSet.Remove(gasto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
