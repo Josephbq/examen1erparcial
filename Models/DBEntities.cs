e73f4aa3793aa85153f1a2bd0be0cd1a6753713d
barra <barra@LAPTOP-U81S3TP5>
2021-09-14 13:44:02 -0400

    new Connection/modelenti

---
+++ Models/DBEntities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class DBEntities:DbContext
+    {
+        public DbSet<Moneda> Monedas { get; set; }
+        public DbSet<Categorias> Categorria { get; set; }
+        public DbSet<Gasto> Gastos { get; set; }
+        public DbSet<Lugar> Lugars { get; set; }
+    }
+}
