c2d254528d6a0b94267cfdb8eaaac62d8e54d220
barra <barra@LAPTOP-U81S3TP5>
2021-09-14 13:37:08 -0400

    models para Gasto Categoria Lugar y Moneda

---
+++ Models/Categorias.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace WebApplication1.Models
+{
+    public class Categorias
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
---
+++ Models/Gasto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Gasto
+    {
+        public int ID { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Cantidad { get; set; }
+        public int CategoriaID { get; set; }
+        public int LugarID { get; set; }
+        public int MonedaID { get; set; }
+    }
+}
